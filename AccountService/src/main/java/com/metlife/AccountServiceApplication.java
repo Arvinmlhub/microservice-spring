ea36892057d53d879759eeb007fbbdec0ab011ed
Arvind Verma <arvindv7238@gmail.com>
2024-03-13 08:59:26 +0530

    account service changess

--- AccountService/src/main/java/com/metlife/AccountServiceApplication.java
+++ AccountService/src/main/java/com/metlife/AccountServiceApplication.java
@@ -2,13 +2,16 @@ package com.metlife;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CurrencyConversionApplication.class, args);
+        SpringApplication.run(AccountServiceApplication.class, args);
 
 
     }
